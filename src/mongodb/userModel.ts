5cd35e1a00a5e1d0f37a2ec09ead8314d2d8d7b1
Evan K <ekerik220@gmail.com>
2020-07-26 17:15:57 +0900

    userModel: username is now not unique, and handle is unique

--- src/mongodb/userModel.ts
+++ src/mongodb/userModel.ts
@@ -11,7 +11,6 @@ const userSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
-    unique: true,
     maxLength: 50,
   },
   password: {
@@ -27,6 +26,7 @@ const userSchema = new mongoose.Schema({
   handle: {
     type: String,
     required: true,
+    unique: true,
   },
 });
 
